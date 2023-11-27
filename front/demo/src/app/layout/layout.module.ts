de18333c03cb23768c9e490f13a8e3180ec4b091
OLIVO MACIAS WALTHER <wolivo@sasf.local>
2023-11-27 17:33:29 -0500

    Loading front

---
+++ front/demo/src/app/layout/layout.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule
+  ]
+})
+export class LayoutModule { }
